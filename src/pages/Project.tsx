673bfb140247646b485098097af0339d3f5b5832
eaaldark <mix7reload@gmail.com>
2022-07-15 00:04:58 -0500

    Create Project.tsx

---
+++ src/pages/Project.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProjectDom = (props: any) => {
+  return (
+    <div className="w-full h-full text-white">
+      <h1>Proyectos</h1>
+    </div>
+  );
+};
+
+export default ProjectDom;
