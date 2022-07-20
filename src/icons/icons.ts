b70ac94d5b96ec3416e13673a36cfc601d1f56f6
eaaldark <mix7reload@gmail.com>
2022-07-19 20:59:38 -0500

    Se añaden Iconos

---
+++ src/icons/icons.ts
@@ -0,0 +1,8 @@
+import Instagram from "./icons8/icons8-instagram.svg";
+import Telegram from "./icons8/icons8-telegram.svg";
+import Gmail from "./icons8/icons8-gmail.svg";
+import Github from "./icons8/icons8-github.svg";
+
+const icons = { Instagram, Telegram, Gmail, Github };
+
+export default icons;
