487e591e415a3d256b4ec9281b07f273b9ca5b5a
eaaldark <mix7reload@gmail.com>
2022-07-19 21:01:42 -0500

    Update Project.tsx

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import { CubeTransparentIcon } from "@heroicons/react/solid";
 
 const ProjectDom = (props: any) => {
   return (
-    <div className="w-full h-full text-white">
-      <h1>Proyectos</h1>
+    <div className="w-full h-full my-auto flex justify-center text-white">
+      <div className="text-center space-y-4">
+        <h1 className="font-bold text-2xl">Proyectos</h1>
+        <div className="">
+          <CubeTransparentIcon className="w-60" />
+        </div>
+        <p>En progreso...</p>
+      </div>
     </div>
   );
 };
