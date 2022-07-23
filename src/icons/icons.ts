2b460f69165a8218c89ecb4d2aa79d5c459efca8
eaaldark <mix7reload@gmail.com>
2022-07-22 22:27:58 -0500

    Update icons.ts

--- src/icons/icons.ts
+++ src/icons/icons.ts
@@ -2,7 +2,8 @@ import Instagram from "./icons8/icons8-instagram.svg";
 import Telegram from "./icons8/icons8-telegram.svg";
 import Gmail from "./icons8/icons8-gmail.svg";
 import Github from "./icons8/icons8-github.svg";
+import Repair from "./svgrepo/repair-svgrepo-com.svg";
 
-const icons = { Instagram, Telegram, Gmail, Github };
+const icons = { Instagram, Telegram, Gmail, Github, Repair };
 
 export default icons;
