5f3abc006cf839a192075fe3ef817481249fd56c
eaaldark <mix7reload@gmail.com>
2022-08-05 23:18:23 -0500

    Se añade tarjeta para poder mostrar los elementos de un array con datos

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -4,18 +4,16 @@ import { openInNewTab } from "../utils/openLink";
 const ProjectDom = (props: any) => {
   return (
     <div className="w-full h-full my-auto flex justify-center text-white">
-      <div className="p-5 space-y-5">
+      <div className="space-y-5">
         <h1 className="font-bold text-2xl text-center">Proyectos</h1>
 
-        <div className="grid grid-project gap-5">
+        <div className="grid grid-project gap-6 p-5 xl:px-16">
           {projects.map((item: any) => {
             return (
               <div className="grid gap-2 border border-white/10">
                 <div className="w-full h-32 items-center justify-center bg-white"></div>
                 <div className="text-center px-5 space-y-2">
-                  <h4 className="font-semibold text-lg">
-                    {item.title}
-                  </h4>
+                  <h4 className="font-semibold text-lg">{item.title}</h4>
                   <p>{item.paragraph}</p>
                 </div>
                 <div className="p-5">
