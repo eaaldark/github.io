a1f77851a915dad9852b98ccf0c7f53a38dd5aff
eaaldark <mix7reload@gmail.com>
2022-08-06 12:26:09 -0500

    Se remueve BrowserRouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Paths from "./routes";
 
 import NavbarDom from "../components/Navbar";
 const RouterDom = (props: any) => {
   console.log("Usando Router");
   return (
-    <BrowserRouter>
+    <>
       <NavbarDom />
       <Routes>
         {(Paths || []).map((item: any, idx: any) => (
           <Route key={item.id} path={item.path} element={<item.component />} />
         ))}
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
